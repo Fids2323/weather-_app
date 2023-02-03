498fc90300b0b80114db339146cf3d63648e08a7
Dmitry Homskiy <workfids23@gmail.com>
2023-02-03 21:26:31 +0300

    disign complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
+import Forecast from "./components/forecast";
 import Inputs from "./components/inputs";
+import TemperatureAndDetails from "./components/temperatureAndDetails";
+import TimeAndLocation from "./components/timeAndLocation";
 import TopButtons from "./components/topButtons";
 
 function App() {
@@ -7,9 +10,15 @@ function App() {
 		<div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
 			<TopButtons />
 			<Inputs />
+
+			<TimeAndLocation />
+			<TemperatureAndDetails />
+
+			<Forecast title="hourly forecast" />
+			<Forecast title="daily forecast" />
 		</div>
 	);
 }
 
 export default App;
-//23:20
+//58
---
+++ src/components/timeAndLocation.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function TimeAndLocation() {
+	return (
+		<div>
+			<div className="flex items-center justify-center my-6">
+				<div className="text-xl text-white font-extralight">Tuesday,31 May 2023|Local time: 12:46 PM</div>
+			</div>
+			<div className="flex items-center justify-center my-3">
+				<p className="text-white text-3xl font-medium">Berlin,DE</p>
+			</div>
+		</div>
+	);
+}
+
+export default TimeAndLocation;
